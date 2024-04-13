bd56447da2278c40a3e63cdd2e0707b89628a284
Matthew M <145183361+MattMorrison94@users.noreply.github.com>
2024-04-13 21:00:57 +0930

    Initial Commit

---
+++ Barcode Scanner/BarcodeScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  BarcodeScannerView.swift
+//  Barcode Scanner
+//
+//  Created by Matthew Morrison on 13/4/2024.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
