3014f715012dab8786058c9afe989dc50471bfef
Matthew M <145183361+MattMorrison94@users.noreply.github.com>
2024-04-16 19:54:21 +0930

    created coordinator and updated scanner view removing plaeholder rectangle

---
+++ Barcode Scanner/ScannerView.swift
@@ -0,0 +1,38 @@
+//
+//  ScannerView.swift
+//  Barcode Scanner
+//
+//  Created by Matthew Morrison on 16/4/2024.
+//
+
+import SwiftUI
+
+struct ScannerView: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> ScannerVC {
+        ScannerVC(scannerDelegate: context.coordinator)
+    }
+    
+    func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
+    final class Coordinator: NSObject, ScannerVCDelegate {
+        func didFind(barcode: String) {
+            print(barcode)
+        }
+        
+        func didSurface(error: CameraError) {
+            print(error.rawValue)
+        }
+        
+        
+    }
+    
+}
+
+#Preview {
+    ScannerView()
+}
